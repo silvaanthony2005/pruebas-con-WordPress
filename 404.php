753b70f6c1fc7c11c6144396d4d1088ff6eb2fc4
silvaanthony2005 <silvaanthony2005@gmail.com>
2025-08-23 16:58:07 -0400

    creacion de page 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="error-404">
+    <h1>¡Ups! Página no encontrada (404)</h1>
+    <p>Lo sentimos, la página que buscas no existe o fue movida.</p>
+    <a href="<?php echo home_url(); ?>" class="btn-volver">Volver al inicio</a>
+</section>
+
+<?php get_footer(); ?>

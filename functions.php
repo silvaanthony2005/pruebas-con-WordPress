8f58dde479755acf4e2c942edf18ed2f282526da
silvaanthony2005 <silvaanthony2005@gmail.com>
2025-08-23 18:02:04 -0400

    añadir estilo para su mapeo por wordpress del footer

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php 
     function rutas(){
-        wp_enqueue_style("style", get_stylesheet_uri(), array(), false, "all");
+        wp_register_style("footer-style", get_template_directory_uri() . "/assets/css/footer.css", array(), false, "all");
+        wp_enqueue_style("style", get_stylesheet_uri(), array("footer-style"), false, "all");
     }
 
     add_action("wp_enqueue_scripts", "rutas");
